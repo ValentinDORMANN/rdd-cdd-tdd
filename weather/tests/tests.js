fb584c83f84f29d0460257215db21b8490657237
ValentinDORMANN <valentin.dormann@gmail.com>
2017-02-24 16:19:27 +0100

    add test

--- weather/tests/tests.js
+++ weather/tests/tests.js
@@ -2,10 +2,23 @@
 
 const chai = require('chai');
 chai.should();
+var expect = require("chai").expect;
 
 const weather = require('../index.js');
+const cli = require('../cli.js');
+const assert = require('assert');
 
-describe('Weather', () => {
+/*describe('Weather', () => {
 	it('should get the weather', (done) => {
 	});
-});
+});*/
+describe('GivenTemperatureFahrenheitWhenToCelciusThenConvertCelcius', function(){
+	console.log(cli)
+	it('when input 32 will return 0', function(){
+		assert.equal(cli._toCelcius(32), 0);
+	});
+
+	it('linkTO', function() {
+		expect(cli).to.exist;
+	});
+})
